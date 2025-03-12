0697eefc9fbf83f15e893ea3c62264f74821f71c
n1lima <nicolima1905@gmail.com>
2025-03-12 12:24:20 -0300

    tentando consertar erro no responsive voice

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    if ("speechSynthesis" in window) {
+       let utterance = new SpeechSynthesisUtterance(texto);
+       utterance.lang = "pt-BR";
+       utterance.rate = 1.2;
+       window.speechSynthesis.speak(utterance);
+    } else {
+       console.log("Web Speech API não suportada neste navegador.");
+    }
+    //Colocando funcionalidade na biblioteca responsiveVoice
 }
 
 function exibirMensagemInicial(){
@@ -27,7 +36,7 @@ function verificarChute(){
     
     if(chute == numeroSecreto){
         exibirTextoNaTela('h1', 'Você Acertou!');
-        let palavraTentativas = tentativas > 1? 'tentativas' : 'tenativa';
+        let palavraTentativas = tentativas > 1? 'tentativas' : 'tentativa';
         let mensagemTentativas = (`Você descobriu o número secreto com ${tentativas} ${palavraTentativas}`)
         exibirTextoNaTela('p', mensagemTentativas);
         document.querySelector('#reiniciar').removeAttribute('disabled');
